8f7b2ced9736b95629644e55c03b98a4dc1aefbe
Chris Millar <chris.millar36@gmail.com>
2018-03-10 16:01:54 -0500

    change main to build a grid of connect 4 size and return each of its cells owners. print each owner as red for now

--- src/main/java/com/schrismillar/connect4/DisplaySomeGrid.java
+++ src/main/java/com/schrismillar/connect4/DisplaySomeGrid.java
@@ -1,10 +1,10 @@
 package com.schrismillar.connect4;
 
-import static java.util.Arrays.asList;
-
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
+import com.schrismillar.connect4.model.Grid;
+import com.schrismillar.connect4.model.PlayerId;
 import com.schrismillar.connect4.ui.*;
 
 public class DisplaySomeGrid {
@@ -13,11 +13,22 @@ public class DisplaySomeGrid {
     private static final DisplayCell YELLOW_DISPLAY_CELL = new YellowDisplayCell();
 
     public static void main(String[] args) {
-        List<List<DisplayCell>> cells = new ArrayList<>();
-        cells.add(asList(EMPTY_DISPLAY_CELL, YELLOW_DISPLAY_CELL, EMPTY_DISPLAY_CELL));
-        cells.add(asList(RED_DISPLAY_CELL, RED_DISPLAY_CELL, YELLOW_DISPLAY_CELL));
-        cells.add(asList(YELLOW_DISPLAY_CELL, RED_DISPLAY_CELL, YELLOW_DISPLAY_CELL));
-        DisplayGrid displayGrid = new DisplayGrid(cells);
+        Grid connectFourGrid = new Grid(6, 7);
+        List<List<PlayerId>> playerIds = connectFourGrid.cellOwners();
+        List<List<DisplayCell>> displayCells = transform(playerIds);
+        DisplayGrid displayGrid = new DisplayGrid(displayCells);
         System.out.print(displayGrid.displayValue());
+    }
+
+    private static List<List<DisplayCell>> transform(List<List<PlayerId>> playerIds) {
+        List<List<DisplayCell>> displayCells = new LinkedList<>();
+        playerIds.forEach(row -> {
+            List<DisplayCell> rowCells = new LinkedList<>();
+            row.forEach(playerId -> {
+                rowCells.add(RED_DISPLAY_CELL);
+            });
+            displayCells.add(rowCells);
+        });
+        return displayCells;
     }
 }
