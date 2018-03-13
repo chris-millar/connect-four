e00229045dafeb6d0b391e46d6eb24c47a86588c
Chris Millar <chris.millar36@gmail.com>
2018-03-12 23:21:50 -0400

    add ConsolePrinter#printBlankLine convenience method

--- src/main/java/com/schrismillar/connect4/util/ConsolePrinter.java
+++ src/main/java/com/schrismillar/connect4/util/ConsolePrinter.java
@@ -5,4 +5,8 @@ public class ConsolePrinter {
     public void println(String string) {
         System.out.println(string);
     }
+
+    public void printBlankLine() {
+        println("");
+    }
 }
