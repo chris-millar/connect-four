41323eb7baa70268230a0925689949728679b6a8
Chris Millar <chris.millar36@gmail.com>
2018-03-31 13:28:38 -0400

    introduce PlayerDeterminer that prompts for console input for a player name then returns a human player with that name

---
+++ src/main/java/com/schrismillar/connect4/game/player/PlayerDeterminer.java
@@ -0,0 +1,32 @@
+package com.schrismillar.connect4.game.player;
+
+import com.schrismillar.connect4.model.PlayerId;
+import com.schrismillar.connect4.util.ConsolePrinter;
+import com.schrismillar.connect4.util.ConsoleScanner;
+
+public class PlayerDeterminer {
+    private final ConsolePrinter consolePrinter;
+    private final ConsoleScanner consoleScanner;
+    private final PlayerNameValidator playerNameValidator;
+    private final PlayerFactory playerFactory;
+
+    public PlayerDeterminer(ConsolePrinter consolePrinter, ConsoleScanner consoleScanner,
+                            PlayerNameValidator playerNameValidator, PlayerFactory playerFactory) {
+        this.consolePrinter = consolePrinter;
+        this.consoleScanner = consoleScanner;
+        this.playerNameValidator = playerNameValidator;
+        this.playerFactory = playerFactory;
+    }
+
+    public Player determinePlayerWithId(PlayerId playerId) {
+        consolePrinter.println("Please provide a name for " + playerId + ":");
+
+        String name = consoleScanner.next();
+        if (playerNameValidator.isValid(name)) {
+            return playerFactory.createHumanPlayerWith(playerId, name);
+        } else {
+            consolePrinter.println("INVALID INPUT: Player name must be alphaNumeric characters only.");
+            return determinePlayerWithId(playerId);
+        }
+    }
+}
---
+++ src/test/java/com/schrismillar/connect4/game/player/PlayerDeterminerTest.java
@@ -0,0 +1,73 @@
+package com.schrismillar.connect4.game.player;
+
+import static com.schrismillar.connect4.model.PlayerId.PLAYER_ONE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+
+import com.schrismillar.connect4.util.ConsolePrinter;
+import com.schrismillar.connect4.util.ConsoleScanner;
+
+public class PlayerDeterminerTest {
+    private static final String NAME = "validPlayerName";
+
+    @Mock private ConsolePrinter consolePrinter;
+    @Mock private ConsoleScanner consoleScanner;
+    @Mock private PlayerNameValidator playerNameValidator;
+    @Mock private PlayerFactory playerFactory;
+    @Mock private HumanPlayer humanPlayer;
+
+    private PlayerDeterminer playerDeterminer;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+
+        playerDeterminer = new PlayerDeterminer(consolePrinter, consoleScanner, playerNameValidator, playerFactory);
+    }
+
+    @Test
+    public void determinePlayerReturnsHumanPlayerProvidedIdAndWithCommandLineSpecifiedName() {
+        when(consoleScanner.next()).thenReturn(NAME);
+        when(playerNameValidator.isValid(NAME)).thenReturn(true);
+        when(playerFactory.createHumanPlayerWith(PLAYER_ONE, NAME)).thenReturn(humanPlayer);
+        InOrder consoleOrderVerifier = inOrder(consolePrinter, consoleScanner);
+
+
+        Player player = playerDeterminer.determinePlayerWithId(PLAYER_ONE);
+
+        assertTrue("Returned Player did not have type HumanPlayer.", player instanceof HumanPlayer);
+        assertEquals(humanPlayer, (HumanPlayer) player);
+
+        consoleOrderVerifier.verify(consolePrinter).println("Please provide a name for " + PLAYER_ONE + ":");
+        consoleOrderVerifier.verify(consoleScanner).next();
+    }
+
+    @Test
+    public void determinePlayerPrintsInvalidInputMessageAndRecursesIfInvalidPlayerName() {
+        String invalidName = "not a valid player name";
+        when(consoleScanner.next()).thenReturn(invalidName).thenReturn(NAME);
+        when(playerNameValidator.isValid(invalidName)).thenReturn(false);
+        when(playerNameValidator.isValid(NAME)).thenReturn(true);
+        when(playerFactory.createHumanPlayerWith(PLAYER_ONE, NAME)).thenReturn(humanPlayer);
+        InOrder consoleOrderVerifier = inOrder(consolePrinter, consoleScanner);
+
+
+        Player player = playerDeterminer.determinePlayerWithId(PLAYER_ONE);
+
+        assertTrue("Returned Player did not have type HumanPlayer.", player instanceof HumanPlayer);
+        assertEquals(humanPlayer, (HumanPlayer) player);
+
+        consoleOrderVerifier.verify(consolePrinter, times(1)).println("Please provide a name for " + PLAYER_ONE + ":");
+        consoleOrderVerifier.verify(consoleScanner, times(1)).next();
+        consoleOrderVerifier.verify(consolePrinter, times(1)).println("INVALID INPUT: Player name must be alphaNumeric characters only.");
+        consoleOrderVerifier.verify(consolePrinter, times(1)).println("Please provide a name for " + PLAYER_ONE + ":");
+        consoleOrderVerifier.verify(consoleScanner, times(1)).next();
+    }
+}
