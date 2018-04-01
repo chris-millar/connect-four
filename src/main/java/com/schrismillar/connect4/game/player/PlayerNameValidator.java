598962e4364baacadf058d3a1b3d58f19e0a32f5
Chris Millar <chris.millar36@gmail.com>
2018-03-31 23:44:36 -0400

    add todo to PlayerNameValidator to add validation results of some kind so that appropriate failed validation message is displayed to user

--- src/main/java/com/schrismillar/connect4/game/player/PlayerNameValidator.java
+++ src/main/java/com/schrismillar/connect4/game/player/PlayerNameValidator.java
@@ -4,6 +4,7 @@ public class PlayerNameValidator {
     private static final String VALID_NAME_REGEX = "^[a-zA-Z0-9]+[a-zA-Z0-9 ]*$";
 
     boolean isValid(String name) {
+        //TODO - need to return some sort of validation results that includes valid/not and message why not valid
         return name.matches(VALID_NAME_REGEX) && name.length() <= 15;
     }
 }
