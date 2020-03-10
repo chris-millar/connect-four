0b3501393881ab100d4021f6ad949ded46c70dcf
Chris Millar <chris.millar36@gmail.com>
2020-03-09 23:38:05 -0400

    fix ApplicationTest. It had not been updated when spring boot refactor happened so it did not even match correct sut interface.

--- src/test/java/com/schrismillar/connect4/ApplicationTest.java
+++ src/test/java/com/schrismillar/connect4/ApplicationTest.java
@@ -10,21 +10,18 @@ import org.mockito.Mock;
 
 import com.schrismillar.connect4.game.CommandLineGameOrganizer;
 import com.schrismillar.connect4.game.Game;
-import com.schrismillar.connect4.util.ConsolePrinter;
-import com.schrismillar.connect4.util.ConsoleScanner;
 
 public class ApplicationTest {
 
     @Mock private CommandLineGameOrganizer commandLineGameOrganizer;
-    @Mock private ConsolePrinter consolePrinter;
-    @Mock private ConsoleScanner consoleScanner;
+    @Mock private ShouldPlayNewGame shouldPlayNewGame;
 
     private Application application;
 
     @Before
     public void setUp() {
         initMocks(this);
-        application = new Application(consolePrinter, consoleScanner, commandLineGameOrganizer);
+        application = new Application(commandLineGameOrganizer, shouldPlayNewGame);
     }
 
     @Test
@@ -32,41 +29,21 @@ public class ApplicationTest {
         Game game1 = mock(Game.class);
         Game game2 = mock(Game.class);
         when(commandLineGameOrganizer.setupNewGame()).thenReturn(game1).thenReturn(game2);
-        when(consoleScanner.next()).thenReturn("y").thenReturn("y").thenReturn("n");
-        InOrder orderVerifier = inOrder(consolePrinter, commandLineGameOrganizer);
+        when(shouldPlayNewGame.call()).thenReturn(true).thenReturn(true).thenReturn(false);
+        InOrder orderVerifier = inOrder(shouldPlayNewGame, commandLineGameOrganizer);
 
         application.run();
 
-        orderVerifier.verify(consolePrinter).println("Would you like to play a new game? <y/n>");
         orderVerifier.verify(commandLineGameOrganizer).playGame(game1);
-        orderVerifier.verify(consolePrinter).println("Would you like to play a new game? <y/n>");
         orderVerifier.verify(commandLineGameOrganizer).playGame(game2);
-        orderVerifier.verify(consolePrinter).println("Would you like to play a new game? <y/n>");
     }
 
     @Test
     public void startDoesNotLoopIfYouAnswerNoTheFirstTime() throws Exception {
-        when(consoleScanner.next()).thenReturn("n");
+        when(shouldPlayNewGame.call()).thenReturn(false);
 
         application.run();
 
-        verify(consolePrinter).println("Would you like to play a new game? <y/n>");
-        verify(commandLineGameOrganizer, never()).setupNewGame();
-        verify(commandLineGameOrganizer, never()).playGame(any());
-    }
-
-    @Test
-    public void startPrintsInvalidInputMessageIfYouDoNotRespondWithYesOrNoToPlayingNewGameWillPromptForNewGameAgainUntilValidValueProvided() throws Exception {
-        when(consoleScanner.next()).thenReturn("invalid value").thenReturn("other invalid value").thenReturn("n");
-        InOrder orderVerifier = inOrder(consolePrinter);
-
-        application.run();
-
-        orderVerifier.verify(consolePrinter).println("Would you like to play a new game? <y/n>");
-        orderVerifier.verify(consolePrinter).println("INVALID INPUT: You must answer either <y> or <n>");
-        orderVerifier.verify(consolePrinter).println("Would you like to play a new game? <y/n>");
-        orderVerifier.verify(consolePrinter).println("INVALID INPUT: You must answer either <y> or <n>");
-        orderVerifier.verify(consolePrinter).println("Would you like to play a new game? <y/n>");
         verify(commandLineGameOrganizer, never()).setupNewGame();
         verify(commandLineGameOrganizer, never()).playGame(any());
     }
